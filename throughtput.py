c53bc1cd04307386b3f6c1faeea3943628271c5c
guill <mr.hidemagarium@gmail.com>
2025-05-03 15:27:52 +0200

    Corregido Thr

--- throughtput.py
+++ throughtput.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-route="data_one" #sys.argv[1] # Ruta de la carpeta que contiene los archivos CSV
+route="data"
 key=os.listdir(route)
 key.sort(key=lambda x: int(re.search(r'\d+', x).group())) 
 thr_tot=[]
@@ -12,11 +12,12 @@ off_tot=[]
 for i in key:
     data = pd.read_csv(f"{route}/{i}")
     thr:float=0.0
-    for j in range(0,10): 
+    for j in range(0,10):
       # Obtenr Throughput de medio de cada elemento ConfiguratorA.host0.ethg$o[0].channel
-      thrmean:float=data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.host{j}.ethg$o[0].channel"), "value"].iloc[0] #bps
+      thrmean=float(data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.host{j}.ethg$o[0].channel"), "value"].iloc[0]) #bps
+     
       if (np.isnan(thrmean)!=True ):
-        thr+=(float(thrmean)/1000)# Convertir a Kbps
+        thr+=(thrmean/1000)# Convertir a Kbps
 
     thr_tot.append((thr/10)) # Promedio de throughput
     off_tot.append(int(re.search(r'\d+', i).group())) # Convertir a bps
