46e145088295720f63fe3eff49ddc96cea788f19
guill <mr.hidemagarium@gmail.com>
2025-05-03 13:57:22 +0200

    Latency and Throughput Mejorado

--- latency.py
+++ latency.py
@@ -2,34 +2,45 @@
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
+import os
+import numpy as np
 
-
- #Cada Switch
-    
-key=["data_10","data_100","data_350", "data_500", "data_800", "data_1000","data_1200","data_1300","data_1500"]
+route="data" #sys.argv[1] # Ruta de la carpeta que contiene los archivos CSV
+key=os.listdir(route)
+key.sort(key=lambda x: int(re.search(r'\d+', x).group())) # Ordenar por el número en el nombre del archivo
+print(key)
 dely_tot=[]
 off_tot=[]
 
 
 for i in key:
-    data = pd.read_csv(f"data/{i}.csv")
+    data = pd.read_csv(f"{route}/{i}")
     dely=[]
+    packetSent=[]
+
     #Cada Host
     for j in range(0,10):
       d_mean=float(data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.host{j}.app[1]") & (data["name"] == "endToEndDelay:mean"), "value"].iloc[0] )
-      dely.append(d_mean) 
+      pack=float(data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.host{j}.app[0]") & (data["name"] == "packetSent:count"), "value"].iloc[0] )
+       #paquete enviado por host
+      if (np.isnan(d_mean)!=True and pack!=0):
+        dely.append(d_mean) 
+        packetSent.append(pack*1024*8)
     
     # #switches
-    # for j in [0,1,3,4,5]:
-    #   l_s=data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.switch{j}.eth[1].queue") & (data["name"] == "queueingTime:mean"), "value"].iloc[0] #s
-    #   dely.append(float(l_s))
+    for j in [0,1,3,4,5]:
+      l_s=float(data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.switch{j}.eth[1].queue") & (data["name"] == "queueingTime:mean"), "value"].iloc[0]) #s
+      if l_s!=0:
+        dely.append(float(l_s))
 
-    # for j in range(0,6):
-    #   l_s=data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.router{j}.eth[1].queue") & (data["name"] == "queueingTime:mean"), "value"].iloc[0] #s
-    #   dely.append(float(l_s))
+    for j in range(0,6):
+      l_s=data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.router{j}.eth[1].queue") & (data["name"] == "queueingTime:mean"), "value"].iloc[0] #s
+      if l_s!=0:
+        dely.append(float(l_s))
           
     dely_tot.append(sum(dely)/len(dely)) # Promedio de throughput all red
-    off_tot.append(int(re.search(r'\d+', i).group())*1024*8)
+    off_tot.append(sum(packetSent)/len(packetSent)/100) # Promedio de throughput all red
+    #off_tot.append(int(re.search(r'\d+', i).group())*1024*8)
 
  
 
--- throughtput.py
+++ throughtput.py
@@ -1,24 +1,29 @@
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
+import os
+import numpy as np
 
-key=["data_10","data_100","data_350", "data_500", "data_800", "data_1000","data_1200","data_1300","data_1500"]
+route="data_one" #sys.argv[1] # Ruta de la carpeta que contiene los archivos CSV
+key=os.listdir(route)
+key.sort(key=lambda x: int(re.search(r'\d+', x).group())) 
 thr_tot=[]
 off_tot=[]
 for i in key:
-    data = pd.read_csv(f"data/{i}.csv")
+    data = pd.read_csv(f"{route}/{i}")
     thr:float=0.0
-    off:float=0.0
     for j in range(0,10): 
       # Obtenr Throughput de medio de cada elemento ConfiguratorA.host0.ethg$o[0].channel
       thrmean:float=data.loc[(data["type"] == "scalar") & (data["module"] == f"ConfiguratorA.host{j}.ethg$o[0].channel"), "value"].iloc[0] #bps
-      thr+=(float(thrmean)/1000)# Convertir a Kbps
+      if (np.isnan(thrmean)!=True ):
+        thr+=(float(thrmean)/1000)# Convertir a Kbps
 
     thr_tot.append((thr/10)) # Promedio de throughput
+    off_tot.append(int(re.search(r'\d+', i).group())) # Convertir a bps
     
    
 # Graficar el throughput total
-plt.plot(["10","100", "350", "500", "800","1200", "1000", "1300", "1500"],thr_tot, label="Throughput All", color="red")
+plt.plot(off_tot,thr_tot, label="Throughput All", color="red")
 plt.xlabel("Offload - pps")
 plt.ylabel("Throughput - kbps")
 plt.title("Capacity")
